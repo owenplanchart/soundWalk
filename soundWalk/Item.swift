84fd348cf6bb236ca81367c2f359cef3684b7858
owenplanchart <owenplanchart@gmail.com>
2025-08-24 17:22:28 +0100

    Initial Commit

---
+++ soundWalk/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  soundWalk
+//
+//  Created by Owen on 24/08/2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ soundWalk/soundWalkApp.swift
@@ -0,0 +1,32 @@
+//
+//  soundWalkApp.swift
+//  soundWalk
+//
+//  Created by Owen on 24/08/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct soundWalkApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
